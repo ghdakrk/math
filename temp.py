21a842978e62ca1fd698218bf7f5958f3c99bbac
ghdakrk <ghdakrk@hanmail.net>
2021-11-06 21:59:47 +0900

    g

--- temp.py
+++ temp.py
@@ -1,4 +1,5 @@
 from manim import *
+import manilib
 
 
 # manim temp.py IMG -pqm
@@ -156,64 +157,64 @@ from manim import *
 
 # manim temp.py Test1 -pqm 
 
-class Test1(Scene):
-    def construct(self):
-        sun = Circle(radius=3, color=WHITE, fill_color=RED, fill_opacity=0.5)
-        earth = Circle(radius=0.6, color=BLUE, fill_color=GREEN, fill_opacity=0.75)
-        arrow = always_redraw(lambda : Line(start = sun.get_center(), end=earth.get_center(), buff=0.2))
-
-
-        self.play(Create(VGroup(sun, earth)), run_time=4)
-        self.wait()
-        
-        self.play(sun.animate.to_edge(LEFT), earth.animate.to_edge(RIGHT), run_time=4)
-        self.wait(2)
-        self.play(FadeIn(arrow), run_time=1)
-        self.wait(2)
-        self.play(FadeOut(arrow), run_time=3)
-        self.wait(2)
-
-        self.play(sun.animate.scale(2), run_time=4)
-        self.play(earth.animate.scale(0.9), run_time=4)
-        self.wait(4)
-
-        self.play(sun.animate.scale(0.4), earth.animate.scale(0.5), run_time=4)
-        self.wait(3)
-
-        self.play(sun.animate.move_to(RIGHT), earth.animate.shift(LEFT*1), run_time=3)
-        self.wait()
-        ellipse = Ellipse(width=10, height=7)
-        self.play(MoveAlongPath(earth, ellipse), rate_func=linear, run_time=4)
-        self.play(MoveAlongPath(earth, ellipse), rate_func=linear, run_time=4)
-        self.play(MoveAlongPath(earth, ellipse), rate_func=linear, run_time=4)
-        self.play(MoveAlongPath(earth, ellipse), rate_func=linear, run_time=4)
-        self.wait(7)
-
-        arc1 = Arc(radius=5, start_angle=0, angle=PI/6)
-        arc2 = Arc(radius=5, start_angle=0, angle=PI/4)
-        self.play(MoveAlongPath(earth, arc1), rate_func=linear, run_time=4)
-        self.wait(6)
-        self.play(MoveAlongPath(earth, arc1), rate_func=linear, run_time=5)
-        self.wait(8)
-
-        self.play(earth.animate.shift(DOWN*2.5, LEFT*4.5), sun.animate.shift(RIGHT*4), run_time=3)
-        self.wait(3)
-        ellipse1 = Ellipse(width=10, height=6)
-        self.play(MoveAlongPath(sun, ellipse1), rate_func=linear, run_time=8)
-        self.play(MoveAlongPath(sun, ellipse1), rate_func=linear, run_time=8)
-        self.wait(5)
-
-# class Test(Scene):
+# class Test1(Scene):
 #     def construct(self):
-#         name = Tex('Amedee').to_edge(UL, buff=0.5)
-#         sq = Square(side_length=0.5, fill_color=GREEN, fill_opacity=0.75).shift(LEFT * 3)
-#         tri = Triangle().scale(0.6).to_edge(DR)
+#         sun = Circle(radius=3, color=WHITE, fill_color=RED, fill_opacity=0.5)
+#         earth = Circle(radius=0.6, color=BLUE, fill_color=GREEN, fill_opacity=0.75)
+#         arrow = always_redraw(lambda : Line(start = sun.get_center(), end=earth.get_center(), buff=0.2))
 
-#         self.play(Write(name))
-#         self.play(DrawBorderThenFill(sq), run_time=2)
-#         self.play(Create(tri))
-#         self.wait()
 
-#         self.play(name.animate.to_edge(UR), run_time=2)
-#         self.play(sq.animate.scale(2), tri.animate.to_edge(DL), run_time=3)
+#         self.play(Create(VGroup(sun, earth)), run_time=4)
 #         self.wait()
+        
+#         self.play(sun.animate.to_edge(LEFT), earth.animate.to_edge(RIGHT), run_time=4)
+#         self.wait(2)
+#         self.play(FadeIn(arrow), run_time=1)
+#         self.wait(2)
+#         self.play(FadeOut(arrow), run_time=3)
+#         self.wait(2)
+
+#         self.play(sun.animate.scale(2), run_time=4)
+#         self.play(earth.animate.scale(0.9), run_time=4)
+#         self.wait(4)
+
+#         self.play(sun.animate.scale(0.4), earth.animate.scale(0.5), run_time=4)
+#         self.wait(3)
+
+#         self.play(sun.animate.move_to(RIGHT), earth.animate.shift(LEFT*1), run_time=3)
+#         self.wait()
+#         ellipse = Ellipse(width=10, height=7)
+#         self.play(MoveAlongPath(earth, ellipse), rate_func=linear, run_time=4)
+#         self.play(MoveAlongPath(earth, ellipse), rate_func=linear, run_time=4)
+#         self.play(MoveAlongPath(earth, ellipse), rate_func=linear, run_time=4)
+#         self.play(MoveAlongPath(earth, ellipse), rate_func=linear, run_time=4)
+#         self.wait(7)
+
+#         arc1 = Arc(radius=5, start_angle=0, angle=PI/6)
+#         arc2 = Arc(radius=5, start_angle=0, angle=PI/4)
+#         self.play(MoveAlongPath(earth, arc1), rate_func=linear, run_time=4)
+#         self.wait(6)
+#         self.play(MoveAlongPath(earth, arc1), rate_func=linear, run_time=5)
+#         self.wait(8)
+
+#         self.play(earth.animate.shift(DOWN*2.5, LEFT*4.5), sun.animate.shift(RIGHT*4), run_time=3)
+#         self.wait(3)
+#         ellipse1 = Ellipse(width=10, height=6)
+#         self.play(MoveAlongPath(sun, ellipse1), rate_func=linear, run_time=8)
+#         self.play(MoveAlongPath(sun, ellipse1), rate_func=linear, run_time=8)
+#         self.wait(5)
+
+class Test(Scene):
+    def construct(self):
+        name = Tex('ghdakrk').to_edge(UL, buff=0.5)
+        sq = Square(side_length=0.5, fill_color=GREEN, fill_opacity=0.75).shift(LEFT * 3)
+        tri = Triangle().scale(0.6).to_edge(DR)
+
+        self.play(Write(name))
+        self.play(DrawBorderThenFill(sq), run_time=2)
+        self.play(Create(tri))
+        self.wait()
+
+        self.play(name.animate.to_edge(UR), run_time=2)
+        self.play(sq.animate.scale(2), tri.animate.to_edge(DL), run_time=3)
+        self.wait()
